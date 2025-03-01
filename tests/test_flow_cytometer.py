335faec892d23ce86d0897391b954f55a1d10ab3
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-03-01 23:04:13 +0100

    fix WIP

--- tests/test_flow_cytometer.py
+++ tests/test_flow_cytometer.py
@@ -3,38 +3,38 @@
 
 import pytest
 import numpy as np
-
+from PyMieSim import units
 from PyMieSim.experiment.detector import CoherentMode
 from PyMieSim.experiment.scatterer import Sphere
-from PyMieSim.experiment.source import PlaneWave
+from PyMieSim.experiment.source import Gaussian
 from PyMieSim.experiment import Setup
-from PyMieSim.units import nanometer, degree, AU, RIU, volt, meter
 
 
 def test_get_measure():
-    source = PlaneWave(
-        wavelength=np.linspace(600, 1000, 150) * nanometer,
-        polarization=0 * degree,
-        amplitude=1 * volt / meter,
+    source = Gaussian(
+        wavelength=np.linspace(600, 1000, 150) * units.nanometer,
+        polarization=0 * units.degree,
+        NA=0.2 * units.AU,
+        optical_power=1e-3 * units.watt,
     )
 
     # Configure the spherical scatterer
     scatterer = Sphere(
-        diameter=np.linspace(400, 1400, 10) * nanometer,
+        diameter=np.linspace(400, 1400, 10) * units.nanometer,
         source=source,
-        property=1.4 * RIU,
-        medium_property=1.1 * RIU
+        property=1.4 * units.RIU,
+        medium_property=1.1 * units.RIU
     )
 
     # Configure the detector
     detector = CoherentMode(
         mode_number='LP01',
-        rotation=0 * degree,
-        NA=[0.1] * AU,
+        rotation=0 * units.degree,
+        NA=[0.1] * units.AU,
         polarization_filter=None,
-        gamma_offset=[0, 1] * degree,
-        phi_offset=0 * degree,
-        sampling=100 * AU
+        gamma_offset=[0, 1] * units.degree,
+        phi_offset=0 * units.degree,
+        sampling=100 * units.AU
     )
 
     # Set up and run the experiment
