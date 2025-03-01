b2177c01783c84f0a625c19a80b1149ba12fcc9f
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-03-01 23:15:11 +0100

    fix WIP

--- tests/test_flow_cytometer.py
+++ tests/test_flow_cytometer.py
@@ -11,36 +11,41 @@ from PyMieSim.experiment import Setup
 
 
 def test_get_measure():
-    source = Gaussian(
-        wavelength=np.linspace(600, 1000, 150) * units.nanometer,
+    total_size = 150
+    source = Gaussian.build_sequential(
+        total_size=total_size,
+        wavelength=np.linspace(600, 1000, total_size) * units.nanometer,
         polarization=0 * units.degree,
         NA=0.2 * units.AU,
         optical_power=1e-3 * units.watt,
     )
 
     # Configure the spherical scatterer
-    scatterer = Sphere(
-        diameter=np.linspace(400, 1400, 10) * units.nanometer,
+    scatterer = Sphere.build_sequential(
+        total_size=total_size,
+        diameter=10 * units.nanometer,
         source=source,
         property=1.4 * units.RIU,
         medium_property=1.1 * units.RIU
     )
 
     # Configure the detector
-    detector = CoherentMode(
+    detector = CoherentMode.build_sequential(
         mode_number='LP01',
         rotation=0 * units.degree,
-        NA=[0.1] * units.AU,
-        polarization_filter=None,
-        gamma_offset=[0, 1] * units.degree,
+        NA=0.1 * units.AU,
+        polarization_filter=np.nan * units.degree,
+        gamma_offset=0 * units.degree,
         phi_offset=0 * units.degree,
-        sampling=100 * units.AU
+        sampling=100 * units.AU,
+        cache_NA=0. *  units.AU,
+        total_size=total_size
     )
 
     # Set up and run the experiment
     experiment = Setup(scatterer=scatterer, source=source, detector=detector)
 
-    experiment.get('coupling', drop_unique_level=True, scale_unit=True)
+    experiment.get_sequential('coupling')
 
 
 if __name__ == "__main__":
