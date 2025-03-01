d0ac4bb1170c3943813c63ec2efb2fd3b82548df
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-03-01 22:59:59 +0100

    fix WIP

--- tests/test_flow_cytometer.py
+++ tests/test_flow_cytometer.py
@@ -6,45 +6,24 @@ import numpy as np
 
 from PyMieSim.experiment.detector import CoherentMode
 from PyMieSim.experiment.scatterer import Sphere
-from PyMieSim.experiment.source import Gaussian, PlaneWave
+from PyMieSim.experiment.source import PlaneWave
 from PyMieSim.experiment import Setup
-from PyOptik import Material
-from PyMieSim.units import nanometer, degree, watt, AU, RIU, volt, meter
-
-# Configure the medium and core materials for the sphere
-properties = [Material.silver, Material.fused_silica, 1.4 * RIU]
-medium_properties = [Material.water, 1.1 * RIU]
-
-# List of measures to be tested
-measures = Sphere.available_measure_list
-
-gaussian_source = Gaussian(
-    wavelength=np.linspace(600, 1000, 150) * nanometer,
-    polarization=0 * degree,
-    optical_power=1e-3 * watt,
-    NA=0.2 * AU
-)
-
-planewave_source = PlaneWave(
-    wavelength=np.linspace(600, 1000, 150) * nanometer,
-    polarization=0 * degree,
-    amplitude=1 * volt / meter,
-)
-
-sources = [gaussian_source, planewave_source]
+from PyMieSim.units import nanometer, degree, AU, RIU, volt, meter
 
 
-@pytest.mark.parametrize('medium_property', medium_properties, ids=[f'Medium:{m}' for m in medium_properties])
-@pytest.mark.parametrize('source', sources, ids=[f'Source:{m.__class__.__name__}' for m in sources])
-@pytest.mark.parametrize('property', properties, ids=[f'Property:{m}' for m in properties])
-@pytest.mark.parametrize('measure', measures)
-def test_get_measure(source, measure, property, medium_property):
+def test_get_measure():
+    source = PlaneWave(
+        wavelength=np.linspace(600, 1000, 150) * nanometer,
+        polarization=0 * degree,
+        amplitude=1 * volt / meter,
+    )
+
     # Configure the spherical scatterer
     scatterer = Sphere(
         diameter=np.linspace(400, 1400, 10) * nanometer,
         source=source,
-        property=property,
-        medium_property=medium_property
+        property=1.4 * RIU,
+        medium_property=1.1 * RIU
     )
 
     # Configure the detector
@@ -61,9 +40,7 @@ def test_get_measure(source, measure, property, medium_property):
     # Set up and run the experiment
     experiment = Setup(scatterer=scatterer, source=source, detector=detector)
 
-    experiment.get(measure, drop_unique_level=True, scale_unit=True)
-
-    experiment.get(measure, drop_unique_level=False, scale_unit=True, as_numpy=True)
+    experiment.get('coupling', drop_unique_level=True, scale_unit=True)
 
 
 if __name__ == "__main__":
