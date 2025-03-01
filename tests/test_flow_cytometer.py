d7f283a90ec4c8d95e332b5c85b27682b683ca6b
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-03-01 23:27:53 +0100

    fix WIP

--- tests/test_flow_cytometer.py
+++ tests/test_flow_cytometer.py
@@ -12,10 +12,104 @@ from FlowCyPy.source import BaseBeam
 from PyMieSim.units import Quantity, degree, watt, AU, hertz
 from FlowCyPy.noises import NoiseSetting
 import pandas as pd
+from FlowCyPy import ScattererCollection, Population, GaussianBeam, FlowCell, FlowCytometer, SignalDigitizer
+from FlowCyPy import distribution
 
+@pytest.fixture
+def default_digitizer():
+    """Fixture for creating a default signal digitizer."""
+    return SignalDigitizer(
+        bit_depth=1024,
+        saturation_levels='auto',
+        sampling_rate=5e6 * units.hertz,
+    )
 
-def test_get_measure():
-    total_size = 150
+@pytest.fixture
+def detector_0():
+    """Fixture for creating the first default detector."""
+    return Detector(
+        name='default',
+        numerical_aperture=1 * units.AU,
+        phi_angle=90 * units.degree,
+        responsitivity=1 * units.ampere / units.watt,
+    )
+
+@pytest.fixture
+def detector_1():
+    """Fixture for creating the second default detector."""
+    return Detector(
+        name='default_bis',
+        numerical_aperture=1 * units.AU,
+        phi_angle=90 * units.degree,
+        responsitivity=1 * units.ampere / units.watt,
+    )
+
+@pytest.fixture
+def size_distribution():
+    """Fixture for creating a normal size distribution."""
+    return distribution.Normal(
+        mean=1.0 * units.micrometer,
+        std_dev=0.1 * units.micrometer
+    )
+
+@pytest.fixture
+def ri_distribution():
+    """Fixture for creating a normal refractive index distribution."""
+    return distribution.Normal(
+        mean=1.5 * units.RIU,
+        std_dev=0.1 * units.RIU
+    )
+
+@pytest.fixture
+def population(size_distribution, ri_distribution):
+    """Fixture for creating a default population."""
+    return Population(
+        particle_count=110 * units.particle,
+        size=size_distribution,
+        refractive_index=ri_distribution,
+        name="Default population"
+    )
+
+@pytest.fixture
+def scatterer_collection(population):
+    """Fixture for creating a scatterer collection with a default population."""
+    scatterer = ScattererCollection()
+    scatterer.add_population(population)
+    return scatterer
+
+@pytest.fixture
+def flow_cell():
+    """Fixture for creating a default flow cell."""
+    source = GaussianBeam(
+        numerical_aperture=1 * units.AU,
+        wavelength=1550 * units.nanometer,
+        optical_power=100e-3 * units.watt,
+    )
+
+    return FlowCell(
+        source=source,
+        volume_flow=0.1 * units.microliter / units.second,
+        flow_area=(12 * units.micrometer) ** 2,
+        event_scheme='uniform-sequential'
+    )
+
+@pytest.fixture
+def flow_cytometer(detector_0, detector_1, scatterer_collection, flow_cell, default_digitizer):
+    """Fixture for creating a default Flow Cytometer."""
+    return FlowCytometer(
+        signal_digitizer=default_digitizer,
+        scatterer_collection=scatterer_collection,
+        detectors=[detector_0, detector_1],
+        flow_cell=flow_cell,
+        coupling_mechanism='mie'
+    )
+
+def test_get_measure(flow_cytometer):
+    acquisition = flow_cytometer.get_acquisition(run_time=0.2 * units.millisecond)
+
+    size_list = acquisition.scatterer['Size'].values
+
+    total_size = len(size_list)
     source = Gaussian.build_sequential(
         total_size=total_size,
         wavelength=np.linspace(600, 1000, total_size) * units.nanometer,
