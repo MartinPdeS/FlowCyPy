db0e2a3f9e1f136cb9ba3fc68722f909fffb90c8
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-03 16:30:43 +0200

    update

--- FlowCyPy/cpp/core/interface.cpp
+++ FlowCyPy/cpp/core/interface.cpp
@@ -9,12 +9,14 @@ PYBIND11_MODULE(interface_core, m) {
     m.doc() = "Pybind11 example with no std::vector, only py::array_t<double>";
 
     py::class_<FlowCyPySim>(m, "FlowCyPySim")
-    .def(py::init<const std::vector<double> &, const std::vector<double> &, const std::vector<double>&, const std::vector<double>&, const double>(),
-             py::arg("widths"),
-             py::arg("centers"),
-             py::arg("coupling_power"),
-             py::arg("time_array"),
-             py::arg("background_power")
-        )
-        .def("get_acquisition", &FlowCyPySim::get_acquisition, "Sums background power plus Gaussian pulses for each scatterer.");
+    .def(
+        py::init<const std::vector<double> &, const std::vector<double> &, const std::vector<double>&, const std::vector<double>&, const double>(),
+        py::arg("widths"),
+        py::arg("centers"),
+        py::arg("coupling_power"),
+        py::arg("time_array"),
+        py::arg("background_power")
+    )
+    .def("get_acquisition", &FlowCyPySim::get_acquisition, "Sums background power plus Gaussian pulses for each scatterer.")
+    ;
 }
