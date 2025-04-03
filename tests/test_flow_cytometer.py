272878271d048389e96077d30faa557b37431263
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-03 12:18:30 +0200

    update

--- tests/test_flow_cytometer.py
+++ tests/test_flow_cytometer.py
@@ -1,4 +1,3 @@
-from PyMieSim import experiment
 import numpy as np
 import pytest
 import matplotlib.pyplot as plt
@@ -117,82 +116,6 @@ def flow_cytometer(detector_0, detector_1, scatterer_collection, flow_cell, sour
     )
 
 # ----------------- UNIT TESTS -----------------
-
-
-
-
-def test_before_hand():
-    source = experiment.source.PlaneWave(
-        polarization=[0, 0, 0, 0] * units.degree,
-        amplitude = [1, 1, 1, 1] * units.volt / units.meter,
-        wavelength=[500, 500, 500, 500] * units.nanometer
-    )
-
-    sphere = experiment.scatterer.Sphere(
-        source=source,
-        diameter=[100, 200, 300, 400] * units.nanometer,
-        property=[1.44, 1.44, 1.44, 1.44] * units.RIU,
-        medium_property=[1.1, 1.1, 1.1, 1.1] * units.RIU
-    )
-
-    # detector = experiment.detector.Photodiode(
-    #     NA=[0.2, 0.2, 0.2, 0.2] * units.AU,
-    #     phi_offset=[0, 0, 0, 0] * units.degree,
-    #     gamma_offset=[0, 0, 0, 0] * units.degree,
-    #     cache_NA=[0, 0, 0, 0] * units.AU,
-    #     sampling=[100, 100, 100, 100] * units.AU,
-    #     mode_number=['NC00', 'NC00', 'NC00', 'NC00'],
-    #     polarization_filter=[np.nan, np.nan, np.nan, np.nan] * units.degree,
-    #     rotation=[0, 0, 0, 0] * units.degree
-    # )
-
-    # setup = experiment.Setup(
-    #     source=source,
-    #     scatterer=sphere,
-    #     # detector=detector
-    # )
-
-    # setup.get_sequential('Qsca')
-
-
-def test_before_hand_1():
-    SIZE = 4
-    source = experiment.source.PlaneWave.build_sequential(
-        total_size=SIZE,
-        polarization=0 * units.degree,
-        amplitude = 1 * units.volt / units.meter,
-        wavelength=500 * units.nanometer
-    )
-
-    sphere = experiment.scatterer.Sphere.build_sequential(
-        total_size=SIZE,
-        source=source,
-        diameter=[100, 200, 300, 400] * units.nanometer,
-        property=1.44 * units.RIU,
-        medium_property=1.1 * units.RIU
-    )
-
-    detector = experiment.detector.Photodiode(
-        NA=[0.2, 0.2, 0.2, 0.2] * units.AU,
-        phi_offset=[0, 0, 0, 0] * units.degree,
-        gamma_offset=[0, 0, 0, 0] * units.degree,
-        cache_NA=[0, 0, 0, 0] * units.AU,
-        sampling=[100, 100, 100, 100] * units.AU,
-        mode_number=['NC00', 'NC00', 'NC00', 'NC00'],
-        polarization_filter=[np.nan, np.nan, np.nan, np.nan] * units.degree,
-        rotation=[0, 0, 0, 0] * units.degree
-    )
-
-    setup = experiment.Setup(
-        source=source,
-        scatterer=sphere,
-        detector=detector
-    )
-
-    setup.get_sequential('coupling')
-
-
-
 
 def test_flow_cytometer_acquisition(flow_cytometer):
     """Test if the Flow Cytometer generates a non-zero acquisition signal."""
