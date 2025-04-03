f93e202bb8018d3d8fbf8a91a8f26b021522ebdb
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-03 12:27:04 +0200

    update

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,42 +1,16 @@
 from PyMieSim import experiment
 import numpy as np
 import pytest
+import matplotlib.pyplot as plt
+from unittest.mock import patch
+from FlowCyPy import FlowCytometer, Detector, ScattererCollection, GaussianBeam, TransimpedanceAmplifier
+from FlowCyPy.flow_cell import FlowCell
+from FlowCyPy.signal_digitizer import SignalDigitizer
+from FlowCyPy import distribution
+from FlowCyPy.population import Sphere
 from FlowCyPy import units
-
-
-
-def test_before_hand():
-    source = experiment.source.PlaneWave(
-        polarization=[0, 0, 0, 0] * units.degree,
-        amplitude = [1, 1, 1, 1] * units.volt / units.meter,
-        wavelength=[500, 500, 500, 500] * units.nanometer
-    )
-
-    sphere = experiment.scatterer.Sphere(
-        source=source,
-        diameter=[100, 200, 300, 400] * units.nanometer,
-        property=[1.44, 1.44, 1.44, 1.44] * units.RIU,
-        medium_property=[1.1, 1.1, 1.1, 1.1] * units.RIU
-    )
-
-    detector = experiment.detector.Photodiode(
-        NA=[0.2, 0.2, 0.2, 0.2] * units.AU,
-        phi_offset=[0, 0, 0, 0] * units.degree,
-        gamma_offset=[0, 0, 0, 0] * units.degree,
-        cache_NA=[0, 0, 0, 0] * units.AU,
-        sampling=[100, 100, 100, 100] * units.AU,
-        mode_number=['NC00', 'NC00', 'NC00', 'NC00'],
-        polarization_filter=[np.nan, np.nan, np.nan, np.nan] * units.degree,
-        rotation=[0, 0, 0, 0] * units.degree
-    )
-
-    # setup = experiment.Setup(
-    #     source=source,
-    #     scatterer=sphere,
-    #     # detector=detector
-    # )
-
-    # setup.get_sequential('Qsca')
+from FlowCyPy import peak_locator
+from FlowCyPy import circuits
 
 
 def test_before_hand_1():
