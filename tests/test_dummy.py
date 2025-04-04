76beb5255d46cf9bcee496085f162f54fa9520b3
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-04 13:29:48 +0200

    update

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -2,45 +2,45 @@ import pytest
 
 
 
-from PyMieSim.units import ureg, Quantity # noqa F401
-from PyMieSim.units import *  # noqa F401
-from numpy import sqrt
+# from PyMieSim.units import ureg, Quantity # noqa F401
+# from PyMieSim.units import *  # noqa F401
+# from numpy import sqrt
 
-_scaled_units_str_list = [
-    'watt', 'volt', 'meter', 'second', 'liter', 'hertz', 'ohm', 'ampere'
-]
+# _scaled_units_str_list = [
+#     'watt', 'volt', 'meter', 'second', 'liter', 'hertz', 'ohm', 'ampere'
+# ]
 
-for _units_str in _scaled_units_str_list:
-    for scale in ['femto', 'pico', 'nano', 'micro', 'milli', '', 'kilo', 'mega']:
-        _unit = scale + _units_str
-        globals()[_unit] = getattr(ureg, _unit)
-
-
-joule = ureg.joule
-coulomb = ureg.coulomb
-power = ureg.watt.dimensionality
-kelvin = ureg.kelvin
-celsius = ureg.celsius
-particle = ureg.particle
-degree = ureg.degree
-distance = ureg.meter.dimensionality
-time = ureg.second.dimensionality
-volume = ureg.liter.dimensionality
-frequency = ureg.hertz.dimensionality
-dB = ureg.dB
-pascal = ureg.pascal
-minute = ureg.minute
+# for _units_str in _scaled_units_str_list:
+#     for scale in ['femto', 'pico', 'nano', 'micro', 'milli', '', 'kilo', 'mega']:
+#         _unit = scale + _units_str
+#         globals()[_unit] = getattr(ureg, _unit)
 
 
-# Define a custom unit 'bit_bins'
-ureg.define("bit_bins = ![detector_resolution]")
-bit_bins = ureg.bit_bins
+# joule = ureg.joule
+# coulomb = ureg.coulomb
+# power = ureg.watt.dimensionality
+# kelvin = ureg.kelvin
+# celsius = ureg.celsius
+# particle = ureg.particle
+# degree = ureg.degree
+# distance = ureg.meter.dimensionality
+# time = ureg.second.dimensionality
+# volume = ureg.liter.dimensionality
+# frequency = ureg.hertz.dimensionality
+# dB = ureg.dB
+# pascal = ureg.pascal
+# minute = ureg.minute
 
-ureg.define("sqrt_hertz = Hz**0.5")
-sqrt_hertz = ureg.sqrt_hertz
+
+# # Define a custom unit 'bit_bins'
+# ureg.define("bit_bins = ![detector_resolution]")
+# bit_bins = ureg.bit_bins
+
+# ureg.define("sqrt_hertz = Hz**0.5")
+# sqrt_hertz = ureg.sqrt_hertz
 
 
-
+from FlowCyPy import units
 
 
 
@@ -49,7 +49,7 @@ sqrt_hertz = ureg.sqrt_hertz
 
 from PyMieSim import experiment as _PyMieSim
 import numpy
-from FlowCyPy import units
+# from FlowCyPy import units
 
 def test_basic():
 
