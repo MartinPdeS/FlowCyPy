6549103061cf7ede5e4c8312f14d2a6b6af3be40
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-04 17:00:38 +0200

    update

--- FlowCyPy/cytometer.py
+++ FlowCyPy/cytometer.py
@@ -12,7 +12,7 @@ from FlowCyPy.acquisition import Acquisition
 from FlowCyPy.signal_digitizer import SignalDigitizer
 from FlowCyPy.helper import validate_units
 from FlowCyPy import dataframe_subclass
-from FlowCyPy.circuits import SignalProcessor
+# from FlowCyPy.circuits import SignalProcessor
 from FlowCyPy.source import BaseBeam
 # from FlowCyPy.binary import interface_core
 from FlowCyPy.noises import NoiseSetting
