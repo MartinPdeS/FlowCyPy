bb40921e0ab2e2a54ed3d14318c20a5d95c55cd7
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-04 14:30:17 +0200

    update

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -4,40 +4,40 @@ from PyMieSim import experiment as _PyMieSim
 import numpy
 from FlowCyPy import units
 
-# def test_basic():
+def test_basic():
 
 
-#     pms_source = _PyMieSim.source.PlaneWave.build_sequential(
-#         total_size=10,
-#         wavelength=500 * units.nanometer,
-#         polarization=0 * units.degree,
-#         amplitude=1 * units.volt / units.meter
-#     )
+    pms_source = _PyMieSim.source.PlaneWave.build_sequential(
+        total_size=10,
+        wavelength=500 * units.nanometer,
+        polarization=0 * units.degree,
+        amplitude=1 * units.volt / units.meter
+    )
 
-#     pms_scatterer = _PyMieSim.scatterer.Sphere.build_sequential(
-#         total_size=10,
-#         diameter=1 * units.nanometer,
-#         property=1.5 * units.RIU,
-#         medium_property=1.0 * units.RIU,
-#         source=pms_source
-#     )
+    pms_scatterer = _PyMieSim.scatterer.Sphere.build_sequential(
+        total_size=10,
+        diameter=1 * units.nanometer,
+        property=1.5 * units.RIU,
+        medium_property=1.0 * units.RIU,
+        source=pms_source
+    )
 
-#     pms_detector = _PyMieSim.detector.Photodiode.build_sequential(
-#         total_size=10,
-#         mode_number='NC00',
-#         NA=0.2 * units.AU,
-#         cache_NA=0.0 * units.AU,
-#         gamma_offset=0.0 * units.degree,
-#         phi_offset=0.0 * units.degree,
-#         polarization_filter=numpy.nan * units.degree,
-#         sampling=200 * units.AU,
-#         rotation=0 * units.degree
-#     )
+    pms_detector = _PyMieSim.detector.Photodiode.build_sequential(
+        total_size=10,
+        mode_number='NC00',
+        NA=0.2 * units.AU,
+        cache_NA=0.0 * units.AU,
+        gamma_offset=0.0 * units.degree,
+        phi_offset=0.0 * units.degree,
+        polarization_filter=numpy.nan * units.degree,
+        sampling=200 * units.AU,
+        rotation=0 * units.degree
+    )
 
 
-#     experiment = _PyMieSim.Setup(source=pms_source, scatterer=pms_scatterer, detector=pms_detector)
+    experiment = _PyMieSim.Setup(source=pms_source, scatterer=pms_scatterer, detector=pms_detector)
 
-#     coupling_value = experiment.get_sequential('coupling')
+    coupling_value = experiment.get_sequential('coupling')
 
 
 def test_flow_cytometer_acquisition():
