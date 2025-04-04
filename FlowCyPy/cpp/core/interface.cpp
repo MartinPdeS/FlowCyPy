b7b5c76f79a12122a85ff2209164193392810244
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-04 10:54:38 +0200

    update

--- FlowCyPy/cpp/core/interface.cpp
+++ FlowCyPy/cpp/core/interface.cpp
@@ -27,31 +27,10 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <iostream>
+#include "core.h"
+
 
 namespace py = pybind11;
-
-
-
-class FlowCyPySim {
-    public:
-        std::vector<double> widths;
-        std::vector<double> centers;
-        std::vector<double> coupling_power;
-        std::vector<double> time_array;
-        double background_power;
-
-        // Constructor taking a py::array_t<double> by value.
-        FlowCyPySim(
-            const std::vector<double> &widths,
-            const std::vector<double> &centers,
-            const std::vector<double> &coupling_power,
-            const std::vector<double> &time_array,
-            const double background_power
-        );
-
-        py::array_t<double> get_acquisition();
-
-};
 
 
 
