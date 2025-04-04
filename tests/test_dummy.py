ccccd9375463668d7cf025dcd6f42987cd7e0700
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-04 11:47:49 +0200

    update

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,9 +1,12 @@
 import pytest
 
 def test_before_hand():
+    import numpy
     from FlowCyPy.binary.interface_core import FlowCyPySim
 
-    FlowCyPySim()
+    a = numpy.linspace(0, 1)
+
+    FlowCyPySim(a, a, a, a, 2)
 
 
 if __name__ == '__main__':
