de11fe2a5a6131e8f8fe165cb1ffbe5e7b61805e
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-04-04 11:42:44 +0200

    update

--- FlowCyPy/cpp/core/core.cpp
+++ FlowCyPy/cpp/core/core.cpp
@@ -1,55 +1,3 @@
-// #include "core.h"
-// #include <stdexcept>
-
-// #include <pybind11/pybind11.h>
-// #include <pybind11/stl.h>
-// #include <pybind11/numpy.h>
-// namespace py = pybind11;
-
-// FlowCyPySim::FlowCyPySim(
-//     const py::array_t<double> &widths,
-//     const py::array_t<double> &centers,
-//     const py::array_t<double> &coupling_power,
-//     const py::array_t<double> &time_array,
-//     const double background_power
-// ) : widths(widths), centers(centers), coupling_power(coupling_power), time_array(time_array), background_power(background_power)
-// {
-//     if (this->widths.size() != this->centers.size() || this->widths.size() != this->coupling_power.size())
-//         throw std::runtime_error("widths, centers, coupling_power must have the same length.");
-// }
-
-// py::array_t<double> FlowCyPySim::get_acquisition() {
-//     // Create output array, initialized to background_power.
-//     py::array_t<double> total_power(time_array.size());
-//     auto r = total_power.mutable_unchecked<1>();
-
-//     for (size_t i = 0; i < time_array.size(); ++i)
-//         r(i) = background_power;
-
-//     // Parallelize the outer loop over particles.
-//     #pragma omp parallel for
-//     for (size_t i = 0; i < widths.size(); ++i) {
-//         double inv_denom = 1.0 / (2.0 * this->widths[i] * this->widths[i]);
-
-//         for (size_t t_idx = 0; t_idx < time_array.size(); ++t_idx) {
-//             double dt = time_array[t_idx] - this->centers[i];
-//             double gauss_val = this->coupling_power[i] * std::exp(- (dt * dt) * inv_denom);
-//             // Use atomic update to avoid race conditions
-//             #pragma omp atomic
-//             r(t_idx) += gauss_val;
-//         }
-//     }
-//     return total_power;
-// }
-
-
-
-
-
-
-
-
-
 #include "core.h"
 #include <stdexcept>
 
@@ -72,8 +20,6 @@ FlowCyPySim::FlowCyPySim(
 }
 
 py::array_t<double> FlowCyPySim::get_acquisition() {
-
-
     size_t
         time_size = time_array.request().size,
         n_pulses = widths.request().size;
--- FlowCyPy/cpp/core/interface.cpp
+++ FlowCyPy/cpp/core/interface.cpp
@@ -1,42 +1,14 @@
-// #include <pybind11/pybind11.h>
-// #include "core.h"
-
-// namespace py = pybind11;
-
-
-// // PYBIND11 MODULE
-// PYBIND11_MODULE(interface_core, m) {
-//     m.doc() = "Pybind11 example with no py::array_t, only py::array_t<double>";
-
-//     py::class_<FlowCyPySim>(m, "FlowCyPySim")
-//     .def(
-//         py::init<const py::array_t<double> &, const py::array_t<double> &, const py::array_t<double>&, const py::array_t<double>&, const double>(),
-//         py::arg("widths"),
-//         py::arg("centers"),
-//         py::arg("coupling_power"),
-//         py::arg("time_array"),
-//         py::arg("background_power")
-//     )
-//     .def("get_acquisition", &FlowCyPySim::get_acquisition, "Sums background power plus Gaussian pulses for each scatterer.")
-//     ;
-// }
-
-
-
-// minimal_function.cpp
 #include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
 #include "core.h"
-
 
 namespace py = pybind11;
 
 
+// PYBIND11 MODULE
 PYBIND11_MODULE(interface_core, m) {
     m.doc() = "Pybind11 example with no py::array_t, only py::array_t<double>";
 
     py::class_<FlowCyPySim>(m, "FlowCyPySim")
-    .def(py::init<>())
     .def(
         py::init<const py::array_t<double> &, const py::array_t<double> &, const py::array_t<double>&, const py::array_t<double>&, const double>(),
         py::arg("widths"),
@@ -45,7 +17,6 @@ PYBIND11_MODULE(interface_core, m) {
         py::arg("time_array"),
         py::arg("background_power")
     )
+    .def("get_acquisition", &FlowCyPySim::get_acquisition, "Sums background power plus Gaussian pulses for each scatterer.")
     ;
-
-    py::module::import("numpy");
 }
